5576c3e954e658097427074d2bf7d9244625a713
hhuezo <hugo.alex.huezo@gmail.com>
2023-04-26 09:45:30 -0600

    git commit -m "second commit"

--- calcular/conexion/conexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-@session_start();
-//$con=new mysqli('localhost','proyectos_ow','proyectos2021','seguimiento_proyectos');
-$con=new mysqli('localhost','root','','seguimiento_proyectos');
-$con->set_charset('utf-8');
- ?>
