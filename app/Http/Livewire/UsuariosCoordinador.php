1141d701c156f249cf4247831b8d388c063a9bd4
hhuezo <hugo.alex.huezo@gmail.com>
2023-08-21 09:21:57 -0600

    reparacion de error de filtro de usuarios

--- app/Http/Livewire/UsuariosCoordinador.php
+++ app/Http/Livewire/UsuariosCoordinador.php
@@ -10,7 +10,7 @@ class UsuariosCoordinador extends Component
     public $busqueda;
     public function render()
     {
-        $usuarios = User::whereNotIn('id', [1, 3, 6, 7, 25,13])->where('unidad_id', '=', auth()->user()->id)
+        $usuarios = User::whereNotIn('id', [1, 3, 6, 7, 25,13])->where('unidad_id', '=', auth()->user()->unidad_id)
             //->where('user_name', 'like', '%' . $this->busqueda . '%')
             ->where('name', 'like', '%' . $this->busqueda . '%')
             ->get();
