464ea6cceec6112687c0cac6d04db0f55836245b
hhuezo <hugo.alex.huezo@gmail.com>
2024-01-19 13:37:00 -0600

    graficos

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\Redirect;
 
 class DashboardController extends Controller
 {
+    public function __construct()
+    {
+          $this->middleware('auth');
+    }
     public function index()
     {
 
