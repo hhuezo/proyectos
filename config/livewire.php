c55e5c7b2554cd210e933ce50b9b399f741849f3
hhuezo <hugo.alex.huezo@gmail.com>
2024-02-08 14:15:20 -0600

    merge mgodoy

--- config/livewire.php
+++ config/livewire.php
@@ -55,7 +55,7 @@ return [
     */
 
     //'asset_url' => 'http://localhost:81/pg/public',
-    'asset_url' => 'http://localhost/proyectos/public',
+    'asset_url' => 'http://localhost/pg/public',
 
     /*
     |--------------------------------------------------------------------------
